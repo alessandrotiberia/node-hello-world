92dbaddabaca3001221a5ac6af4a1a4b0bf5caee
Alessandro Tiberia <tiberia7030@gmail.com>
2026-05-20 15:55:27 +0200

    punto 3 pnpm start

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * Creiamo la prima applicazione con NodeJs e inizializziamola con pnpm init --init-type=module,
+Scriviamo un file index.js che dovrà stampare nel terminale “Hello World”. Proviamo ad eseguirlo dal terminale stesso usando i comandi di node base.,
+Impostiamo ora uno script "start" in package.json e facciamo in modo di lanciare il nostro script con pnpm start,
+Impostiamo un nuovo script "watch" in package.json che possa essere lanciato con pnpm watch e
+ che aggiorni in tempo reale le modifiche ai nostri file. Lanciamolo
+  e proviamo a cambiare il nostro codice in modo che stampi nel terminale “Hello Boolean”. 
+  Dovremmo vedere il terminale senza fermare e rilanciare il server.
+ */
+
+  console.log("hello world");
+
+  // pnpm start va su package json e legge start dentro "scripts" e dice:
+  //leggi il file index js 
+  
+
